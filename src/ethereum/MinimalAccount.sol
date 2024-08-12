d15a188000bc14d71e9b6d8422863f7c9d3c98a5
naman mohnani <mohnaninaman@gmail.com>
2024-08-13 01:09:53 +0530

    erc4337

---
+++ src/ethereum/MinimalAccount.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {IAccount} from "lib/account-abstraction/contracts/interfaces/IAccount.sol";
+import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
+
+
+contract MinimalAccount is IAccount {
+    function validateUserOp(PackedUserOperation memory userOp, bytes32 userOpHash, uint256 missingAccountFunds)
+        external
+        returns (uint256 validationData)
+    {}
+}
