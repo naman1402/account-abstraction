8578d09775d4ab62d9aeb9004f4a9b7737620c78
naman mohnani <mohnaninaman@gmail.com>
2024-08-13 16:20:48 +0530

    minimal account contract

--- src/ethereum/MinimalAccount.sol
+++ src/ethereum/MinimalAccount.sol
@@ -4,11 +4,85 @@ pragma solidity ^0.8.24;
 
 import {IAccount} from "lib/account-abstraction/contracts/interfaces/IAccount.sol";
 import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {SIG_VALIDATION_FAILED, SIG_VALIDATION_SUCCESS} from "lib/account-abstraction/contracts/core/Helpers.sol";
+import {IEntryPoint} from "lib/account-abstraction/contracts/interfaces/IEntryPoint.sol";
 
 
-contract MinimalAccount is IAccount {
-    function validateUserOp(PackedUserOperation memory userOp, bytes32 userOpHash, uint256 missingAccountFunds)
+
+contract MinimalAccount is IAccount, Ownable {
+
+    error MinimalAccount__NotFromEntryPoint();
+    error MinimalAccount__NotFromEntryPointOrOwner();
+    error MinimalAccount__CallFailed(bytes);
+
+    IEntryPoint private immutable entryPoint;
+
+    modifier fromEntryPoint() {
+        if(msg.sender != address(entryPoint)) {
+            revert MinimalAccount__NotFromEntryPoint();
+        }
+        _;
+    }
+
+    modifier fromEntryPointOrOwner() {
+        if(msg.sender != address(entryPoint) && msg.sender != owner()) {
+            revert MinimalAccount__NotFromEntryPointOrOwner();
+        }
+        _;
+    }
+    constructor(address _entryPoint) Ownable(msg.sender) {
+        entryPoint = IEntryPoint(_entryPoint);
+    }
+
+    function validateUserOp(PackedUserOperation calldata userOp, bytes32 userOpHash, uint256 missingAccountFunds)
         external
+        fromEntryPoint
         returns (uint256 validationData)
-    {}
+    {
+        // struct PackedUserOperation { 
+            // address sender; our minimal account
+            // uint256 nonce; nonce
+            // bytes initCode; IGNORE
+            // bytes callData; IMPORTANT STUFF
+            // bytes32 accountGasLimits;
+            // uint256 preVerificationGas;
+            // bytes32 gasFees;
+            // bytes paymasterAndData;
+            // bytes signature;
+        validationData = _validateSignature(userOp, userOpHash);
+        _payPrefund(missingAccountFunds);
+        
+    }
+
+    function execute(address dest, uint256 value, bytes calldata functionData) external fromEntryPoint {
+        (bool success, bytes memory result) = dest.call{value: value}(functionData);
+        if(!success) {
+            revert MinimalAccount__CallFailed(result);
+        }
+    }
+
+    function _validateSignature(PackedUserOperation calldata userOp, bytes32 userOpHash) internal view returns (uint256 validationData) {
+        bytes32 ehtSignedMessageHash = MessageHashUtils.toEthSignedMessageHash(userOpHash);
+        address signer = ECDSA.recover(ehtSignedMessageHash, userOp.signature);
+        if(signer != owner()) {
+            return SIG_VALIDATION_FAILED;
+        }
+        return SIG_VALIDATION_SUCCESS;
+    }
+
+    function _payPrefund(uint256 missingAccountFund) internal {
+        if(missingAccountFund != 0) {
+            (bool success, ) = payable(msg.sender).call{value: missingAccountFund, gas:type(uint256).max}("");
+            (success);
+        }
+    }
+
+    function getEntryPoint() external view returns (address) {
+        return address(entryPoint);
+    }
+
+    receive() external payable{}
 }
