429dd439c6168508d8b89d5951dcd42a2742462f
naman mohnani <mohnaninaman@gmail.com>
2024-08-23 14:49:32 +0530

    zksync

---
+++ src/zksync/ZkMinimalAccount.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {IAccount} from "lib/foundry-era-contracts/src/system-contracts/contracts/interfaces/IAccount.sol";
+import {Transaction} from "lib/foundry-era-contracts/src/system-contracts/contracts/libraries/MemoryTransactionHelper.sol";
+import {SystemContractsCaller} from "lib/foundry-era-contracts/src/system-contracts/contracts/libraries/SystemContractsCaller.sol";
+import {
+    NONCE_HOLDER_SYSTEM_CONTRACT,
+    BOOTLOADER_FORMAL_ADDRESS,
+    DEPLOYER_SYSTEM_CONTRACT
+} from "lib/foundry-era-contracts/src/system-contracts/contracts/Constants.sol";
+import {INonceHolder} from "lib/foundry-era-contracts/src/system-contracts/contracts/interfaces/INonceHolder.sol";
+
+import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+
+// SYSTEM CONTRACTS IN ZK-SYNC !!!!!!!!!!!
+
+contract ZkMinimalAccount is IAccount, Ownable {
+
+    constructor() Ownable(msg.sender) {}
+
+    receive() external payable {}
+
+    function validateTransaction(bytes32 _txHash, bytes32 _suggestedSignedHash, Transaction calldata _transaction)
+        external
+        payable
+        returns (bytes4 magic)
+    {
+        _validateTransaction(_transaction);
+    }
+
+    function executeTransaction(bytes32 _txHash, bytes32 _suggestedSignedHash, Transaction calldata _transaction)
+        external
+        payable
+    {}
+
+    function executeTransactionFromOutside(Transaction calldata _transaction) external payable {}
+
+    function payForTransaction(bytes32 _txHash, bytes32 _suggestedSignedHash, Transaction calldata _transaction)
+        external
+        payable
+    {}
+
+    function prepareForPaymaster(bytes32 _txHash, bytes32 _possibleSignedHash, Transaction calldata _transaction)
+        external
+        payable
+    {}
+
+    function _validateTransaction(Transaction memory _transaction) internal returns (bytes4 magic) {
+        SystemContractsCaller.systemCallWithPropagatedRevert(uint32(gasleft()), address(NONCE_HOLDER_SYSTEM_CONTRACT), 0, abi.encodeCall(INonceHolder.incrementMinNonceIfEquals, (_transaction.nonce)));
+        uint256 totalRequiredBalance = _transaction.totalRequiredBalance();
+        if(totalRequiredBalance > address(this).balance) {
+            revert ();
+        }
+
+        bytes32 txHash = _transaction.encodeHash();
+        address signer = ECDSA.recover(txHash, _transaction.signature);
+        bool isValidSigner = signer == owner();
+        if (isValidSigner) {
+            magic = ACCOUNT_VALIDATION_SUCCESS_MAGIC;
+        } else {
+            magic = bytes4(0);
+        }
+
+        return magic;
+    }
+
+    function _executeTransaction(Transaction memory _transaction) internal {}
+}
